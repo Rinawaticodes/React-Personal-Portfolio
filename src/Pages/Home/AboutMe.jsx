a9728205b9834f8151780c8ccdad351e8ae33fce
Rinawaticodes <kyurei.kim@gmail.com>
2024-07-29 12:54:33 +0200

    add component for portfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+  return (
+    <section className="about--section" id="aboutMe">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="" />
+      </div>
+      <div className="hero--section--content--box--about--sectin--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills--section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            nemo error laudantium minima explicabo quas officia commodi est
+            laboriosam deleniti dolore in placeat tenetur eveniet unde ipsa
+            itaque delectus nostrum.
+          </p>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            nemo error laudantium minima explicabo quas officia commodi est
+            laboriosam deleniti dolore in placeat tenetur eveniet unde ipsa
+            itaque delectus nostrum.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey I am Rina</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Full stack</span>
+            <br />
+            Developper
+          </h1>
+          <p className="hero--section--description">
+            Young Developpeur
+            <br />
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get in Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="" />
+      </div>
+    </section>
+  );
+}
